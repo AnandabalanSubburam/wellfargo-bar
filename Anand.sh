340ac683c3d0ec09aeb83550dff9a80d7256e036
AnandabalanSubburam <51076830+AnandabalanSubburam@users.noreply.github.com>
2019-05-27 14:45:22 +0530

    Update Anand.sh

--- Anand.sh
+++ Anand.sh
@@ -6,4 +6,4 @@ echo "Cricket Worldcup 2019">>anand.txt
 a=`cat anand.txt`
 echo $a
 echo $Environment for $variable >> anand.txt
-echo "Demo from Anand Branch"
+echo "Demo from Anand Branch updated"
